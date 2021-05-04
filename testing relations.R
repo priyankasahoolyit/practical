2b584da0f49473345b188894950a3866e42b4257
priyankasahoolyit <L00151175@student.lyit.ie>
2021-05-04 14:34:03 +0100

    Initial Commit

---
+++ testing relations.R
@@ -0,0 +1,20 @@
+# use statistical methods to examine 
+# the relationship between our variables of interest
+
+# beavers dataset
+# contains the body temp of 4 beavers captured every 10 mins
+# over a day
+# We want to examne difference in average body temp
+# to evaluate whether body temp effected by activity
+
+#H0: mean beaver temperature is not affected by activity
+#H1: mean beaver temperature is affected by activity
+
+?beavers
+str(beaver2)
+
+# vars we will need
+# we will evaluate beaver temp and activity
+
+# temp = continuous variable
+# activity = categorical dichotomos variable
